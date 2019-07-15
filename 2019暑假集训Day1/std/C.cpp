ccd0a50c9c9c75fdcfe69bf21adde54ad77583c5
HiedanoAkyuu <ctzguozi@163.com>
2019-07-15 16:42:31 +0800

    Update C.cpp

--- 2019暑假集训Day1/std/C.cpp
+++ 2019暑假集训Day1/std/C.cpp
@@ -1,29 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char s[105],tmp[105];
-
 int main()
 {
-    scanf("%s",s);
-    int ans=0,n=strlen(s);
-    for(int i=0;i<n;i++)
-        for(int j=i;j<n;j++)
-        {
-            int t=0;
-            memset(tmp,0,sizeof(tmp));
-            for(int k=i;k<=j;k++) tmp[k-i]=s[k];
-            char tmp1[105];
-            for(int k=0;k<n-(j-i);k++)
-            {
-                memset(tmp1,0,sizeof(tmp));
-                for(int l=k;l<k+(j-i+1);l++)
-                    tmp1[l-k]=s[l];
-                if(strcmp(tmp1,tmp)==0) t++;
-            }
-            if(t>=2) ans=max(ans,j-i+1);
-        }
-    printf("%d",ans);
-    return 0;
+	string s;
+	bool flag=false;
+	cin>>s;
+	int len=s.size();
+	for(int i=len;i>=0;i--)
+	{
+		for(int j=0;j<len;j++)
+	 {
+		 if(s.rfind(s.substr(j,i))!=j)//rfind（和find的区别是查找最后一次出现的位置）
+	 	{	
+			cout<<i;
+			flag=true;
+			break; 
+		 }
+	 }
+	 if(flag) break;
+	}	 	 
+	if(!flag) cout<<0;
+	return 0;
 }
-
